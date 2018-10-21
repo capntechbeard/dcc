916fab55007f2863c2a57cc4e5f7d9a575a34aca
capntechbeard <capntechbeard@users.noreply.github.com>
2018-10-21 16:53:24 -0500

    added views

---
+++ src/views/A04Profession.jsx
@@ -0,0 +1,23 @@
+import React, { Component, Fragment } from 'react';
+import Button from '../components/Button/Button'
+import Container from '../components/Container/Container'
+import Header from '../components/Header/Header'
+
+class AProfession extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container title="Container Title">
+          <Header title="Header" />
+          <Button 
+            className="next-button"
+            isDisabled
+            text="NEXT" />
+        </Container>
+      </Fragment>
+        
+    );
+  }
+}
+
+export default AProfession;
