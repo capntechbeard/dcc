0580806186379fa42bf4c2071d166bb356d47acd
capntechbeard <capntechbeard@gmail.com>
2019-03-27 14:08:58 -0500

    Update A01AbilityScores.jsx

--- src/views/A01AbilityScores.jsx
+++ src/views/A01AbilityScores.jsx
@@ -25,77 +25,77 @@ class AAbilityScores extends Component {
     return (
       <Fragment>
         <div className="text-align-center">
-        <Container>
-          <Header title="Ability Scores" />
-          
-          <Distributor
-            ability="Strength"
-            nextCost={abilities.str.nextCost}
-            handleIncrement={handleAbilityIncrement}
-            handleDecrement={handleAbilityDecrement}
-            buttonClass="ability-str"
-            value={abilities.str.val}
-          />
-          <Distributor
-            ability="Endurance"
-            nextCost={abilities.end.nextCost}
-            handleIncrement={handleAbilityIncrement}
-            handleDecrement={handleAbilityDecrement}
-            buttonClass="ability-end"
-            value={abilities.end.val}
-          />
-          <Distributor
-            ability="Agility"
-            nextCost={abilities.agi.nextCost}
-            handleIncrement={handleAbilityIncrement}
-            handleDecrement={handleAbilityDecrement}
-            buttonClass="ability-agi"
-            value={abilities.agi.val}
-          />
-          <Distributor
-            ability="Knowledge"
-            nextCost={abilities.kno.nextCost}
-            handleIncrement={handleAbilityIncrement}
-            handleDecrement={handleAbilityDecrement}
-            buttonClass="ability-kno"
-            value={abilities.kno.val}
-          />
-          <Distributor
-            ability="Wisdom"
-            nextCost={abilities.wis.nextCost}
-            handleIncrement={handleAbilityIncrement}
-            handleDecrement={handleAbilityDecrement}
-            buttonClass="ability-wis"
-            value={abilities.wis.val}
-          />
-          <Distributor
-            ability="Charisma"
-            nextCost={abilities.cha.nextCost}
-            handleIncrement={handleAbilityIncrement}
-            handleDecrement={handleAbilityDecrement}
-            buttonClass="ability-cha"
-            value={abilities.cha.val}
-          />
-          
+          <Container>
+            <Header title="Ability Scores" />
+            
+            <Distributor
+              ability="Strength"
+              nextCost={abilities.str.nextCost}
+              handleIncrement={handleAbilityIncrement}
+              handleDecrement={handleAbilityDecrement}
+              buttonClass="ability-str"
+              value={abilities.str.val}
+            />
+            <Distributor
+              ability="Endurance"
+              nextCost={abilities.end.nextCost}
+              handleIncrement={handleAbilityIncrement}
+              handleDecrement={handleAbilityDecrement}
+              buttonClass="ability-end"
+              value={abilities.end.val}
+            />
+            <Distributor
+              ability="Agility"
+              nextCost={abilities.agi.nextCost}
+              handleIncrement={handleAbilityIncrement}
+              handleDecrement={handleAbilityDecrement}
+              buttonClass="ability-agi"
+              value={abilities.agi.val}
+            />
+            <Distributor
+              ability="Knowledge"
+              nextCost={abilities.kno.nextCost}
+              handleIncrement={handleAbilityIncrement}
+              handleDecrement={handleAbilityDecrement}
+              buttonClass="ability-kno"
+              value={abilities.kno.val}
+            />
+            <Distributor
+              ability="Wisdom"
+              nextCost={abilities.wis.nextCost}
+              handleIncrement={handleAbilityIncrement}
+              handleDecrement={handleAbilityDecrement}
+              buttonClass="ability-wis"
+              value={abilities.wis.val}
+            />
+            <Distributor
+              ability="Charisma"
+              nextCost={abilities.cha.nextCost}
+              handleIncrement={handleAbilityIncrement}
+              handleDecrement={handleAbilityDecrement}
+              buttonClass="ability-cha"
+              value={abilities.cha.val}
+            />
+            
 
-          <Button 
-            className="back-button"
-            onClick={this.handleBackNavigation}
-            text="BACK" />
+            <Button 
+              className="back-button"
+              onClick={this.handleBackNavigation}
+              text="BACK" />
 
-          <PointDisplay 
-          className="a01-score"
-            score={abilities.points}/>
+            <PointDisplay 
+            className="a01-score"
+              score={abilities.points}/>
 
-          <Button 
-            className="next-button"
-            // isDisabled={abilities.points !== 0}
-            onClick={this.handleNavigation}
-            text="NEXT" />
+            <Button 
+              className="next-button"
+              // isDisabled={abilities.points !== 0}
+              onClick={this.handleNavigation}
+              text="NEXT" />
 
 
 
-        </Container>
+          </Container>
         </div>
       </Fragment>
         
